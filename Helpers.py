f2dca90136c8327f304dbb1b562b813a6928ddd0
Jack Zhang <junbojack@gmail.com>
2023-05-30 12:37:26 +0800

    allow bot to pin, unpin message and annouce results

--- Helpers.py
+++ Helpers.py
@@ -1,4 +1,5 @@
 from datetime import timedelta
+import re
 
 def next_weekday(d, weekday):
   days_ahead = weekday - d.weekday()
@@ -28,11 +29,28 @@ def retrieve_chat_id(update, context=None):
       raise KeyError('Unable to retrieve chat_id')
 
 def retrieve_message_id(update, context):
+  # bot will access if there is a reply_to_message involved
   try:
     return update.effective_message.reply_to_message.message_id
   except AttributeError:
+    # if not bot will retrieve the message_id from stored data
     try:
       poll_id = latest_poll_id(context.bot_data)
       return context.bot_data[poll_id]['message_id']
     except ValueError:
       return None
+
+def process_poll_results(options):
+  result_dict = {}
+  for option in options:
+    result_dict[option.text] = option.voter_count
+
+  result = ''
+  for key, value in dict(sorted(result_dict.items(), key=lambda x:x[1], reverse = True)).items():
+    result += f"\n{key}: {value}"
+
+  return result
+
+def extract_training_week(question_str):
+  pattern = "\[(.*)\]"
+  return re.search(pattern, question_str).group()
--- main.py
+++ main.py
@@ -20,9 +20,19 @@ mute = True
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
   await context.bot.send_message(
+    chat_id=Helper.retrieve_chat_id(update),
+    text="PE Class 🤖 at your service, ready to take training attendance!"
+  )
+
+  Helper.store_chat_id(update, context)
+
+async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
+  await context.bot.send_message(
     chat_id = Helper.retrieve_chat_id(update),
-    text = "I'm a bot, now ready to take training attendance!\n" +
+    text = "Glad you asked, here is what I can do!\n" +
            "/poll: generate poll\n" +
+           "/close: close the latest poll or a specific poll that is 'replied to'\n" +
+           "/result: report the latest poll result\n" +
            "/mute: generate annoucements for updates to latest poll\n" +
            "/unmute: stop announcements for updates to latest poll"
   )
@@ -33,15 +43,29 @@ async def close(update: Update, context: ContextTypes.DEFAULT_TYPE):
   chat_id = Helper.retrieve_chat_id(update)
   message_id = Helper.retrieve_message_id(update, context)
 
+  # if unable to retrieve message_id, bot will look at pinned message for final reference
+  if not message_id:
+    chat = await context.bot.get_chat(chat_id)
+    if chat.pinned_message.from_user.is_bot == True and chat.pinned_message.poll:
+      message_id = chat.pinned_message.message_id
+
+  # closing a poll will unpin the poll and annouce results
   if message_id:
-    await context.bot.stop_poll(
+    message = await context.bot.stop_poll(
       chat_id,
       message_id
     )
 
+    await context.bot.unpin_chat_message(
+      chat_id,
+      message_id
+    )
+
+    results = Helper.process_poll_results(message.options)
     await context.bot.send_message(
       chat_id,
-      text='Nothing to see here, poll is now closed!'
+      f"Poll is now closed for {Helper.extract_training_week(message.question)}!" +
+      f"{results}"
     )
 
 async def poll(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -54,6 +78,20 @@ async def poll(update: Update, context: ContextTypes.DEFAULT_TYPE):
     is_anonymous=False,
     allows_multiple_answers=True,
   )
+
+  chat = await context.bot.get_chat(chat_id)
+
+  # always try to unpin previous poll to prevent unreliabile results fetching
+  if chat.pinned_message:
+    await context.bot.unpin_chat_message(
+      chat_id,
+      chat.pinned_message.message_id
+    )
+
+  await context.bot.pin_chat_message(
+    chat_id,
+    message.message_id
+  )
   # Save some info about the poll the bot_data for later use in receive_poll_answer
   payload = {
     message.poll.id: {
@@ -63,6 +101,21 @@ async def poll(update: Update, context: ContextTypes.DEFAULT_TYPE):
     }
   }
   context.bot_data.update(payload)
+
+async def result(update: Update, context: ContextTypes.DEFAULT_TYPE):
+  # result only generated from latest pinned poll
+  # does not work on reply_to_message
+  chat_id = Helper.retrieve_chat_id(update, context)
+  chat = await context.bot.get_chat(chat_id)
+  if chat.pinned_message.from_user.is_bot == True and chat.pinned_message.poll:
+    results = Helper.process_poll_results(chat.pinned_message.poll.options)
+
+    await context.bot.send_message(
+      chat_id,
+      f"Here's the latest poll result for {Helper.extract_training_week(chat.pinned_message.poll.question)}" +
+      f"{results}",
+      parse_mode=ParseMode.HTML
+    )
 
 async def receive_poll_answer(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Summarize a users poll vote"""
@@ -88,9 +141,9 @@ async def receive_poll_answer(update: Update, context: ContextTypes.DEFAULT_TYPE
       # looks at bot_data for latest poll id
       # only allow poll updates for the latest poll
       if answer.poll_id != Helper.latest_poll_id(context.bot_data):
-          return
+        return
       else:
-          questions = answered_poll['questions']
+        questions = answered_poll['questions']
 
       selected_options = answer.option_ids
       answer_string = ''
@@ -153,11 +206,13 @@ if __name__ == '__main__':
     # assign bot commands to code functions
     # /poll to async def poll
     application.add_handler(CommandHandler('start', start))
-    application.add_handler(CommandHandler('close', close))
     application.add_handler(CommandHandler('poll', poll))
+    application.add_handler(CommandHandler('close', close))
+    application.add_handler(CommandHandler('result', result))
     application.add_handler(CommandHandler('mute', mute_bot))
     application.add_handler(CommandHandler('unmute', unmute_bot))
     application.add_handler(CommandHandler('aware', aware))
+    application.add_handler(CommandHandler('help', help))
     application.add_handler(MessageHandler(filters.TEXT, handle_message))
     application.add_handler(PollAnswerHandler(receive_poll_answer))
     application.add_error_handler(error)
